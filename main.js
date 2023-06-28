bdf984b0dcb9f3d6365efb33d8b8a067356994da
suatozdemir <Suatozdemir88@gmail.com>
2023-06-28 15:01:00 +0100

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ var ajaxCall = (key, url, prompt) => {
       data: JSON.stringify({
         model: "gpt-3.5-turbo",
         prompt: prompt,
-        max_tokens: 1024,
+        max_tokens: 2048,
         n: 1,
         temperature: 0.5,
       }),
